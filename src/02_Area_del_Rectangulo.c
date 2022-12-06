7c9adb32db8233ce41cc571eda055ba4107ad956
criss1611 <118156092+criss1611@users.noreply.github.com>
2022-12-06 12:17:25 -0500

    Clase 6/12/2022

---
+++ src/02_Area_del_Rectangulo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* @author: Cris
+* @date: 6.dic.2022
+* @details: Determinar el area de un rectangulo
+*/
+int main()
+{   //Declarar e inicializar variables
+    int longitud=0, ancho=0, areaRect=0;
+    printf("Ingrese la Longitud: ");
+    scanf("%i", &longitud);
+
+    printf("Ingrese el Ancho: ");
+    scanf("%i", &ancho);
+
+    areaRect= longitud * ancho;
+
+    printf("El Area del Rectangulo es: %i \n",areaRect);
+    printf("El Area del Rectangulo de Ancho %i y Longitud %i es: %i \n",ancho, longitud, areaRect);
+
+    return 0;
+}
